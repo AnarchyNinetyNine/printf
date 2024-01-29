539985ab806d0d2cb613b2c27550a58b736a0da3
Idris <franckenstein999@gmail.com>
2024-01-29 08:44:15 -0800

    another main.h modification

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
 int _fetch_specifier(char format);
 void _format_sc(va_list list, const char *fmt, int *charCount);
 int _strlen(char *s);
@@ -12,4 +11,4 @@ int _printf(const char *format, ...);
 void _puts(char *s);
 int _putchar(char c);
 
-#endif
+#endif /* _MAIN_H_ */
