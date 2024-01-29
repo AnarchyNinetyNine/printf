e187e3f6d03583431708f54581ef9bed352253ad
Idris <franckenstein999@gmail.com>
2024-01-28 19:24:48 -0800

    Third try

--- main.h
+++ main.h
@@ -1,10 +1,9 @@
-#ifndef _PRINTF_
-#define _PRINTF_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
 
 int _fetch_specifier(char format);
 void _format_sc(va_list list, const char *fmt, int *charCount);
