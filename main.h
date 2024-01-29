540f0723a1236bb3d015641648a017c3a9d466dd
Idris Elgarrab <125475208+AnarchyNinetyNine@users.noreply.github.com>
2024-01-29 23:06:20 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,17 @@
-#ifndef _PRINTF_
-#define _PRINTF_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
-#include <stddef.h>
 #include <stdio.h>
+
 int _fetch_specifier(char format);
 void _format_sc(va_list list, const char *fmt, int *charCount);
 int _strlen(char *s);
 int _printf(const char *format, ...);
 void _puts(char *s);
 int _putchar(char c);
-#endif /* _PRINTF_ */
+
+#endif /* _MAIN_H_ */
